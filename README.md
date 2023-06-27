6f62dc6a1bd088458ed5e69a1da82542fb61867c
sar0868 <safarov-ar@yandex.ru>
2023-06-28 00:43:24 +0300

    homework 2. added exceptions: LowFuelError, NotEnoughFuel, CargoOverload class Vehicle added attributes weight, started, fuel, fuel_consumption and added methods start and move. class Engine: added attributes volume and pistons. class Car: added attribute engine and method set_engine. class Plane: added attributes cargo and ma_cargo and methods load_cargo and remove_all_cargo.

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ email: *safarov-ar@yandex.ru*
 **1. Homework 1**  
 *deadline*: 26.06.2023  
 *start*: 14.06.2023  
+*15.06.2023 в 20:55 Станислав Никуличев принял ДЗ*
+
+**2. Homework 2**  
+*deadline*: 17.07.2023  
+*start*: 27.06.2023
 
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+        self.cargo = 0
+
+    def load_cargo(self, cargo_weight):
+        if self.cargo + cargo_weight > self.max_cargo:
+            msg = 'Machine overload'
+            raise CargoOverload(msg)
+        self.cargo += cargo_weight
+
+    def remove_all_cargo(self):
+        result = self.cargo
+        self.cargo = 0
+        return result
+
