62b74ff3197bb50eee944b9b9a71f55e5571ac85
sar0868 <safarov-ar@yandex.ru>
2023-07-21 08:56:54 +0300

    homework 3 install fastapi and uvicorn, create application on FastAPI, create Dockerfile

--- homework_03/main.py
+++ homework_03/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from view import router as ping_router
 
 app = FastAPI()
-app.include_router(ping_router)
+app.include_router(ping_router, prefix="/ping")
 
 
 @app.get("/")
--- homework_03/view.py
+++ homework_03/view.py
@@ -5,7 +5,7 @@ router = APIRouter(tags=["ping"])
 
 
 @router.get(
-    "/ping/",
+    "/",
     response_model=dict(),
     status_code=status.HTTP_200_OK)
 def get_ping():
