a421ff1f30c25a533c320deb8bfa6ef088d2b413
sar0868 <safarov-ar@yandex.ru>
2023-07-21 23:39:51 +0300

    homework 3 edit view

--- homework_03/view.py
+++ homework_03/view.py
@@ -6,7 +6,7 @@ router = APIRouter(tags=["ping"])
 
 @router.get(
     "/",
-    response_model=dict(),
+    response_model=dict,
     status_code=status.HTTP_200_OK)
 def get_ping():
     return {"message": "pong"}
